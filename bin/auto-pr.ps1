685cafe49dcbcb981b87d8eb3c6c11a11a55c275
Annin <143312793+Anninzy@users.noreply.github.com>
2024-02-29 08:47:51 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "Anninzy/scoop-bucket:main"
+    [String]$upstream = "Anninzy/scoop-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
